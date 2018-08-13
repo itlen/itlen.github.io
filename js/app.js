fe621ecf5c76e9bd66a57d7fddd3c498bf9ef912
itlen <tlen@lenta.ru>
2018-08-13 13:05:36 +0400

    Init commit

---
+++ js/app.js
@@ -0,0 +1,185 @@
+var App = function(elementId){
+	this.wrapper = document.getElementById(elementId);
+	this.post = this.wrapper.querySelector('.posts-wrapper');
+	this.hompage = document.querySelector('main');
+}
+
+App.prototype.setState = function(state){
+	document.body.className = '';
+	document.body.classList.add(state);
+}
+
+App.prototype.active = function(){
+	this.setState('active');
+}
+
+App.prototype.done = function(){
+	this.setState('done');
+}
+
+App.prototype.hide = function(){
+	this.setState('hideapp');
+	this.post.innerHTML = '';
+}
+
+App.prototype.error = function(html){
+	this.setState('done');
+	this.post.innerHTML = html;
+}
+
+App.prototype.bind = function(cssSelectors,events,callback,debug=false){
+	events = events.split(',');
+	elements= this.wrapper.querySelectorAll(cssSelectors)||this;
+	elements.forEach(element=>{
+		events.forEach(event=>{
+			element.addEventListener(event,callback);
+			if (debug) console.dir(element+' listen '+event);
+		});
+	});
+}
+
+App.prototype.renderPost = function (data) {
+
+	if(data.length<1) {
+		this.hide();
+		return;
+	}
+
+	var html;
+
+	if (data.length == 1) {
+
+		html = '<h2 class="post-title">'+data[0].title+'</h2>';
+		html += '<div class="post-body">'+data[0].body+'</div>';
+
+	} else {
+
+		html = '<ul class="posts-list">';
+		data.forEach(i=>{
+			html += '<li class="posts-list_item">';
+			html += '<h2 class="post-title">'+i.title+'</h2>';
+			html += '<div class="post-body">'+i.body+'</div>';
+			html += '<br><a class="read-more" href="post/'+i.id+'">read more</a>';
+			html += '</li>';
+		});
+		html += '</ul>';
+	}
+
+
+	
+	this.post.innerHTML = html;
+
+	this.bind('a','click',(e)=>{
+		e.preventDefault();
+		let href = e.target.getAttribute('href');
+		if ( href != '#') {
+			window.location.hash = '/'+href;
+		}
+	});
+}
+
+App.prototype.getData = function(url){
+
+	let rout = url.split('/').filter(i=>i!='#'&&i!='/'&&i!='');
+
+	if(rout.length<1) {
+		this.hide();
+		return;
+	}
+
+	switch(rout.length){
+		case 1: url='data/'+rout[0]+'.json';break;
+		case 2: url='data/post-'+rout[1]+'.json';break;
+		case 3: url='data/post-'+rout[1]+'-'+rout[2]+'.json';break;
+		default:break;
+	}
+
+	this.active();
+
+	if('caches' in self) {
+		console.clear();
+		console.dir('есть кэши');
+		caches.open('myCache').then(cache=>{
+			cache.match(url).then( response => { 
+				console.dir('кэш резолвится c '+response);
+				if(response.status == 200) {
+					response.json().then(data=>{
+						this.done();
+						console.dir('отдали из кэша');
+						this.renderPost(data);
+					})
+				}
+			}).catch(()=>{
+				console.dir('нет в кэше, идем в сеть');
+				fetch(url)
+					.then(response => {
+						if(response.status == 200) {
+							console.dir('все ок, овтет 200');
+							let resClone = response.clone();
+
+							response.json().then(data=>{
+								this.done();
+								console.dir('отдали из сети');
+								this.renderPost(data);
+							})
+
+							cache.put(url,resClone).then(e=>{
+								console.dir('И положили в кэш '+url+' + '+resClone);
+							});
+
+						} else {
+							console.dir(response.status+' ошибка');
+							this.error('<h2>'+response.status+'</h2><br>Bad url: '+window.location.href);
+						}
+					}).catch((err)=>{
+						msg = '<code>'+err+'<br>';
+						msg += 'Net type: '+navigator.connection.effectiveType+'<br>';
+						msg += 'RTT: '+navigator.connection.rtt+'<br>';
+						msg += 'You online: '+navigator.onLine+'</code><br><br>';
+						msg += 'Проблема с сетью. Ресурс <u>'+url+'</u> еще не в кэше';
+						this.error(msg);
+					});				
+			});
+		});
+	} else {
+
+	}
+}
+
+const app = new App('posts');
+/*************************************************************************/
+
+	app.getData(window.location.hash);
+	
+	window.addEventListener('hashchange',()=>{
+		app.getData(window.location.hash);
+	});
+
+	document.querySelectorAll('a').forEach((item)=>{
+		item.addEventListener('click',function(e){
+			e.preventDefault();
+			let href = this.getAttribute('href');
+			if ( href != '#') {
+				window.location.hash = '/'+href;
+			}	
+		});
+	});
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+function(){
 	Object.prototype.on = function(f,c){return Array.from(this,(i)=>i.addEventListener(f,c));}
 	Object.prototype.find = function(s){return this.querySelectorAll(s);}
 	Object.prototype.first = function(s){if(!s) return this.firstElementChild;return this.querySelector(s);}
@@ -19,6 +20,7 @@
 		}
 		return a;
 	};
+
 	Object.prototype.bind = function(cssSelectors,events,callback,debug=false){
 		events = events.split(',');
 		elements= this.find(cssSelectors)||this;
@@ -29,6 +31,7 @@
 			});
 		});
 	}
+
 	Object.prototype.toggleClass = function(className){
 		try { this.classList.toggle(className);}
 		catch {Array.from(this, (i)=>i.classList.toggle(className));}
@@ -53,122 +56,7 @@
 		return Array.from(this.parentElement.childNodes).filter(i=>i.nodeName != '#text' && i != this && i.matches(s));
 	};
 
-function Wrapper (w,m){
-
-	this.init = function(html){
-		this.active(html);
-		w.bind('a','click',function(e){
-			e.preventDefault();
-			let href = '/'+this.getAttribute('href')
-			if ( href != '/#') {
-				window.location.hash = href;
-			}		
-		});
-	}
-
-	this.active = function(html){
-		m.addClass('hidden');
-		w.addClass('active')
-			.first('.posts-wrapper')
-			.innerHTML = html;
-		w.addClass('done');
-	}
-	
-	this.passive = function(){
-		m.removeClass('hidden');
-		w.removeClass('active')
-			.removeClass('error')
-			.removeClass('done')
-			.first('.posts-wrapper')
-			.innerHTML = "";
-	}
-	
-	this.prepare = function(){
-		this.passive();
-		w.addClass('active');
-	}
 }
-
-var wrapper = new Wrapper(document.first('.posts'),document.first('main'));
-
-
-
-
-function Post (data) {
-	this.html = '<ul class="posts-list">';
-	data.forEach(i=>{
-		this.html += '<li class="posts-list-item"><h2>'+i.title+'</h2><div class="post-spoiler">'+i.body+'</div>';
-		if (data.length>1) this.html += '<a href="post/'+i.id+'">read more</a>';
-		this.html += `</li>`;
-	});
-	this.html += '</ul>';
-	this.render = function () {
-		if(!this.html) {
-			wrapper.passive();
-			return;
-		}
-		wrapper.init(this.html);
-	}
-}
-
-
-
-
-
-
-
-var router = function (url) {
-
-	let rout = url.split('/').filter(i=>i!='#'&&i!='');
-
-	if(rout.length<1) {
-		wrapper.passive();
-		return;
-	}
-
-	switch(rout.length){
-		case 1: getDataFromUrl(rout[0]);break;
-		case 2: getDataFromUrl('post-'+rout[1]);break;
-		case 3: getDataFromUrl(rout[2]);break;
-		default:break;
-	}
-
-	async function getDataFromUrl(url){
-		if(url)
-			await fetch('data/'+url+'.json')
-				.then(res => {
-					if(res.status === 200) {
-						res.json().then(data=>{
-							new Post(data).render();
-						})
-					} else {
-						wrapper.active('<h2>'+res.status+'</h2><p class="font-16">Bad url: '+window.location.href+'</p>');
-					}
-				}).catch((err)=>console.log(err));	
-		return false;
-	}
-}
-
-
-//******************************************************//
-
-	document.bind('a','click',function(e){
-		e.preventDefault();
-		let href = '/'+this.getAttribute('href')
-		if ( href != '/#') {
-			window.location.hash = href;
-		}		
-	});
-
-	document.body.removeClass('hidden');
-	document.body.removeClass('2');
-	router(window.location.hash);
-
-	window.addEventListener('hashchange',()=>{
-		router(window.location.hash);
-	});
-
-
 
 
 
